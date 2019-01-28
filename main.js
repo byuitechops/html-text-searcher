fd2824eba5e4a0d6532b8c744cf9595b953c34fe
Zach Heiner <cardpunk1206@yahoo.com>
2019-01-28 16:02:19 -0700

    working on the file

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-const cherrio = require('cherrio')
+const cheerio = require('cheerio');
+var promiseLimit = require('promise-limit');
+var d3 = require('d3-dsv');
+var fs = require("fs");
+
+var limit = promiseLimit(5);
+
+var contents = fs.readFileSync(process.argv[2]);
+
+var syllabusList = JSON.parse(contents);
+
+async function getHTML(syllabus) {
+    
+}
+
+limit.map(syllabusList, getHTML).then()
+
